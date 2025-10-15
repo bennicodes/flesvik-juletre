6587f4f24a4a8b1af4fcaa1b98793dbb04f46fef
Benedict Notoane <benedict.notoane@hotmail.com>
2025-10-15 15:46:07 +0200

    change navbar styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,25 @@ import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <>
-      <header>
+    <div className={styles.rootContainer}>
+      <header className={styles.header}>
         <div className={styles.navbarContainer}>
           <Navbar />
         </div>
-        
+        <section className={styles.heroSection}>
+          <h1 className={styles.heroHeading}>Flesvik Juletre</h1>
+          <div className={styles.heroIntro}>
+            <h3 className={styles.introHeading}>
+              Finn ditt perfekte juletre – Selvhogst i Feiring eller
+              selvbetjening på Eidsvoll Verk.
+            </h3>
+          </div>
+          {/* <div className={styles.}></div> */}
+        </section>
       </header>
       <main></main>
       <footer></footer>
-    </>
+    </div>
   );
 }
 
