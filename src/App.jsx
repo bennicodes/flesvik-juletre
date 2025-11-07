09c57f92959ca5c7cd91f172bb088f5635b8f900
Benedict Notoane <benedict.notoane@hotmail.com>
2025-11-07 23:18:45 +0100

    fix typo

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
 
             <h3 className={styles.introHeading}>
               Finn ditt perfekte juletre – selvhogst i Feiring, selvbetjening på
-              Eidsvoll Verk, eller få det levert rett på hjem i ukene 49–51.
+              Eidsvoll Verk, eller få det levert rett hjem i ukene 49–51.
             </h3>
 
             <div className={styles.buttonGroup}>
